464e351f1347d5f0bb64a9bcfcdab80b74540eba
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-25 08:11:37 +0900

    ESLintとPrettierのライブラリ導入と設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,15 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    '@typescript-eslint',
+    'simple-import-sort',
+  ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -21,5 +26,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/consistent-type-imports': 'error',
   },
-};
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+}
