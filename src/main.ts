471647e71381cd277c2a0e1793909bc401fc8bb2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-28 18:01:10 +0900

    main.tsの作成

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
-import { Request } from 'express'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
     credentials: true,
     origin: ['http://localhost:3000'],
