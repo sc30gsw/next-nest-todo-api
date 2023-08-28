e2cecb877100b0d173619c3a9577efbf25472a78
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-28 14:48:38 +0900

    CORS対策・Cookieを扱う記述・validationPipeをグローバルに宣言する処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+import { Request } from 'express'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+  app.use(cookieParser())
   await app.listen(4000)
 }
 bootstrap()
