e17bd9e752bf9801ac8c289e73dffc48583031ed
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-28 14:48:00 +0900

    グローバルに値を扱うConfigModuleを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module'
+import { PrismaModule } from './prisma/prisma.module'
+import { TodoModule } from './todo/todo.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    TodoModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
